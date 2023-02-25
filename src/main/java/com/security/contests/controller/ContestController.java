11a8ab810ace69389989f835b0954d9c9cfc6f5f
nikhil Kumar <mail2nikhilkumar3@gmail.com>
2023-02-26 00:45:13 +0530

    adding changes.

--- src/main/java/com/security/contests/controller/ContestController.java
+++ src/main/java/com/security/contests/controller/ContestController.java
@@ -1,5 +1,6 @@
 package com.security.contests.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,11 +8,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.security.contests.domain.User;
+import com.security.contests.repository.ContestRepositry;
+import com.security.contests.repository.ContestantRepositry;
+import com.security.contests.repository.JudgeRepositry;
+import com.security.contests.repository.LedgerRepositry;
+import com.security.contests.repository.RoleRepository;
+import com.security.contests.repository.SponserRepositry;
+import com.security.contests.repository.UserRepository;
+import com.security.contests.repository.UserRoleRepositry;
+import com.security.contests.repository.WalletRepositry;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ContestController {
+
+	@Autowired
+	private ContestantRepositry contestantRepositry;
+
+	@Autowired
+	private ContestRepositry contestRepositry;
+
+	@Autowired
+	private JudgeRepositry JudgeRepositry;
+
+	@Autowired
+	private LedgerRepositry LedgerRepositry;
+
+	@Autowired
+	private RoleRepository RoleRepository;
+
+	@Autowired
+	private SponserRepositry SponserRepositry;
+
+	@Autowired
+	private UserRepository UserRepository;
+
+	@Autowired
+	private UserRoleRepositry UserRoleRepositry;
+
+	@Autowired
+	private WalletRepositry WalletRepositry;
 
 	@GetMapping("/home")
 	public String test(HttpServletRequest request, Model model, @AuthenticationPrincipal User user) {
@@ -25,8 +62,18 @@ public class ContestController {
 
 	@PostMapping(value = "/refreshData")
 	public String referData(HttpServletRequest request, Model model) throws Exception {
-		
+		WalletRepositry.deleteAll();
+		LedgerRepositry.deleteAll();
+		contestRepositry.deleteAll();
+		contestantRepositry.deleteAll();
+		JudgeRepositry.deleteAll();
+		SponserRepositry.deleteAll();
+		UserRepository.deleteAll();
+		UserRoleRepositry.deleteAll();
+		RoleRepository.deleteAll();
 		model.addAttribute("isAdmin", true);
+		model.addAttribute("refresh", true);
+
 		return "home";
 	}
 }
